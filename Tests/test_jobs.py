5d2ebf643e19e1f8eba9a6d1d78b88f52e7990ae
mshrestha <mihir.xtha@gmail.com>
2020-02-07 23:53:36 -0500

    Major overhaul for sprint 2. Still need to fix tests. Commiting to access code in another device.

--- Tests/test_jobs.py
+++ Tests/test_jobs.py
@@ -1,94 +1,13 @@
 import jobs
 import os.path
-import sqlite3
 import random
+import sqlite3
 
 
 # Simple test function that checks if the function returns a list with over 100 items.
 def test_retrieve_jobs():
     result = jobs.retrieve_jobs()
     assert len(result) > 100
-
-
-# Simple test function that checks if the jobs are being saved to the database.
-def test_save_to_database():
-    # Checking to see if the database exists, so we can delete it to check
-    # if the save_to_database function actually creates the database table.
-    databaseFileName = 'jobs.db'
-    retrievedJobs = jobs.retrieve_jobs()
-    if os.path.exists(databaseFileName):
-        os.remove(databaseFileName)
-
-    # Running the save_to_database function with the retrieve_jobs function to check if it creates the table.
-    jobs.save_to_database(retrievedJobs)
-    assert os.path.exists('jobs.db')
-
-    # Checking if the database has some values that should be expected there. In this case, I know
-    # that there is a job where the title is 'Lead Data Acquisition Design Engineer'.
-    # UPDATE: It also picks a random one from the retrieved list now and checks if it exists in the database.
-    testTitle1 = random.choice(retrievedJobs)['title']
-    testTitle2 = "Lead Data Acquisition Design Engineer"
-    connection = sqlite3.connect('jobs.db')
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT * FROM jobs WHERE jobs.Title = ?", (testTitle1,))
-    assert cursor.fetchone()
-    cursor.execute("SELECT * FROM jobs WHERE jobs.Title = ?", (testTitle2,))
-    assert cursor.fetchone()
-
-    connection.close()
-
-
-# Simple test function that adds good new data to a database then checks if it exists in the database.
-def test_add_to_database_with_good_data():
-    # Adding good data to the database.
-    additionalTestData = [
-        {"id": "TESTID1",
-         "type": "Full Time",
-         "url": "test1.com",
-         "created_at": "Sat Feb 01 12:53:40 UTC 2020",
-         "company": "TESTCOMPANY",
-         "company_url": "test1.com",
-         "location": "Boston, MA",
-         "title": "Senior Python/Django Developer ",
-         "description": "Test Description",
-         "how_to_apply": "Yes.",
-         "company_logo": "test.png"}]
-
-    # Checking if the new data is added to the database.
-    jobs.save_to_database(additionalTestData)
-    connection = sqlite3.connect('jobs.db')
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM jobs WHERE jobs.id = 'TESTID1'")
-    assert cursor.fetchone()
-    connection.close()
-
-
-# Simple test function that tries to add bad new data to a database then checks if it is updated accordingly.
-def test_add_to_database_with_bad_data():
-    # Adding bad data to the database.
-    additionalTestData = {
-            "id": "TESTID2",
-            "type": "Part Time",
-            "url": "test1.com"}
-
-    # Checking if the new data is added to the database.
-    jobs.save_to_database(additionalTestData)
-    connection = sqlite3.connect('jobs.db')
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM jobs WHERE jobs.id = 'TESTID2'")
-    assert cursor.fetchone() is None
-    connection.close()
-
-    # Throwing in some additional weird arguments to test the function.
-    jobs.save_to_database(102391)
-    jobs.save_to_database('follow')
-    jobs.save_to_database(['break'])
-    jobs.save_to_database((1, 2, 3))
-    jobs.save_to_database('')
-    jobs.save_to_database([])
-    jobs.save_to_database([[]])
-    jobs.save_to_database([{'id': 'test23', "type": "full-time"}])
 
 
 # Simple test function that checks if the function actually writes the JSON data by first checking
@@ -98,7 +17,7 @@ def test_add_to_database_with_bad_data():
 # UPDATE: It picks a random one from the retrieved list now and checks if it exists in the .txt file.
 def test_dump_data():
     retrievedJobs = jobs.retrieve_jobs()
-    # testTitle = 'Full Stack Software Engineer - Rails'
+    testTitle = 'Full Stack Software Engineer - Rails'
     fileName = 'json.txt'
     if os.path.exists(fileName):
         os.remove(fileName)
@@ -106,14 +25,129 @@ def test_dump_data():
     jobs.dump_data(retrievedJobs, fileName)
     assert os.path.exists(fileName)
 
-    testTitle = random.choice(retrievedJobs)['title']
+    randomTestTitle = random.choice(retrievedJobs)['title']
 
     with open(fileName, 'r') as openFile:
         fileLines = openFile.readlines()
         assert len(fileLines) > 0
         match = False
+        randomMatch = False
         for fileLine in fileLines:
             if testTitle in fileLine:
                 match = True
+            if randomTestTitle in fileLine:
+                randomMatch = True
+            if match and randomMatch:
                 break
-        assert match
+        assert match and randomMatch
+
+
+# Simple function that checks if a database is being created.
+def test_open_db():
+    # Checking to see if the database exists, so we can delete it to check
+    # if the open_db function actually creates the database table.
+    databaseFileName = 'jobs.db'
+    if os.path.exists(databaseFileName):
+        os.remove(databaseFileName)
+
+    connection, cursor = jobs.open_db(databaseFileName)
+    assert os.path.exists('jobs.db')
+    jobs.close_db(connection)
+
+
+# Simple function that checks if the correct columns are being created in a table. In this case, I know
+# that the description column exists. So, I'll try to add the column. Of course, it should fail. So if it fails,
+# the test passes, and if it doesn't fail and actually ends up adding the column, the test fails.
+def test_create_table():
+    databaseFileName = 'jobs.db'
+    connection, cursor = jobs.open_db(databaseFileName)
+    jobs.create_table(connection, cursor)
+    columnExists = False
+
+    try:
+        cursor.execute('ALTER TABLE jobs ADD COLUMN Description;')
+    except sqlite3.OperationalError:
+        columnExists = True
+
+    assert columnExists
+
+
+# Simple test function that checks if the jobs are being saved to the database.
+# def test_save_to_database():
+#
+#     databaseFileName = 'jobs.db'
+#     tableName = 'jobs'
+#     retrievedJobs = jobs.retrieve_jobs()
+#
+#
+#     connection, cursor = jobs.open_db(databaseFileName)
+#
+#     jobs.create_table(tableName, connection, cursor)
+#
+#
+#     # Checking if the database has some values that should be expected there. In this case, I know
+#     # that there is a job where the title is 'Lead Data Acquisition Design Engineer'.
+#     # UPDATE: It also picks a random one from the retrieved list now and checks if it exists in the database.
+#     testTitle1 = random.choice(retrievedJobs)['title']
+#     testTitle2 = "Lead Data Acquisition Design Engineer"
+#     connection = sqlite3.connect('jobs.db')
+#     cursor = connection.cursor()
+#
+#     cursor.execute("SELECT * FROM jobs WHERE jobs.Title = ?", (testTitle1,))
+#     assert cursor.fetchone()
+#     cursor.execute("SELECT * FROM jobs WHERE jobs.Title = ?", (testTitle2,))
+#     assert cursor.fetchone()
+#
+#     connection.close()
+
+
+# Simple test function that adds good new data to a database then checks if it exists in the database.
+# def test_add_to_database_with_good_data():
+#     # Adding good data to the database.
+#     additionalTestData = [
+#         {"id": "TESTID1",
+#          "type": "Full Time",
+#          "url": "test1.com",
+#          "created_at": "Sat Feb 01 12:53:40 UTC 2020",
+#          "company": "TESTCOMPANY",
+#          "company_url": "test1.com",
+#          "location": "Boston, MA",
+#          "title": "Senior Python/Django Developer ",
+#          "description": "Test Description",
+#          "how_to_apply": "Yes.",
+#          "company_logo": "test.png"}]
+#
+#     # Checking if the new data is added to the database.
+#     jobs.save_to_database(additionalTestData)
+#     connection = sqlite3.connect('jobs.db')
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT * FROM jobs WHERE jobs.id = 'TESTID1'")
+#     assert cursor.fetchone()
+#     connection.close()
+
+
+# Simple test function that tries to add bad new data to a database then checks if it is updated accordingly.
+# def test_add_to_database_with_bad_data():
+#     # Adding bad data to the database.
+#     additionalTestData = {
+#             "id": "TESTID2",
+#             "type": "Part Time",
+#             "url": "test1.com"}
+#
+#     # Checking if the new data is added to the database.
+#     jobs.save_to_database(additionalTestData)
+#     connection = sqlite3.connect('jobs.db')
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT * FROM jobs WHERE jobs.id = 'TESTID2'")
+#     assert cursor.fetchone() is None
+#     connection.close()
+#
+#     # Throwing in some additional weird arguments to test the function.
+#     jobs.save_to_database(102391)
+#     jobs.save_to_database('follow')
+#     jobs.save_to_database(['break'])
+#     jobs.save_to_database((1, 2, 3))
+#     jobs.save_to_database('')
+#     jobs.save_to_database([])
+#     jobs.save_to_database([[]])
+#     jobs.save_to_database([{'id': 'test23', "type": "full-time"}])
--- jobs.py
+++ jobs.py
@@ -1,11 +1,13 @@
 # Author: Mihir Shrestha
-import requests
-import time
+from requests import get
+from time import sleep
 import json
 import sqlite3
+from typing import Tuple, List, Dict
 
 
-# Main function that calls the retrieve_jobs(), save_to_database() and dump_data() functions.
+# Main function that calls the retrieve_jobs(), open_db(), create_table_jobs(),
+# save_to_database(), and dump_data() functions.
 def main():
     # nice job on sprint one - benign comment to test github actions
     WRITE_TO_FILE = True
@@ -13,7 +15,11 @@ def main():
     jobs = retrieve_jobs()
 
     if UPDATE_DATABASE:
-        save_to_database(jobs)
+        databaseName = 'jobs.db'
+        connection, cursor = open_db(databaseName)
+        create_table(connection, cursor)
+        save_to_database(jobs, connection, cursor)
+        close_db(connection)
 
     if WRITE_TO_FILE:
         fileName = "json.txt"
@@ -23,75 +29,133 @@ def main():
 # Function that retrieves the jobs from GitHub. It has a sleep function implemented,
 # so that the program doesn't shoot all the requests at once to GitHub. It also checks
 # if it received a 200 response code or not and reports the code back to the user if it is not 200.
-def retrieve_jobs():
+# UPDATE: It is now not hard-coded to go up to 5 pages, it keeps looping through until the number
+# of jobs is less than 50. Also, a new failCounter has been implemented to reduce the risks for
+# an infinite loop.
+def retrieve_jobs() -> List[Dict]:
     missedList = []
     jsonData = []
-    for num in range(1, 6):
+
+    endOfJobs = False
+    num = 1
+    failCounter = 0
+    # The failCounter is implemented because if GitHub is down or the page does not exist,
+    # the loop would never break.
+
+    while not endOfJobs:
         url = "https://jobs.github.com/positions.json?page=" + str(num)
-        response = requests.get(url)
+        response = get(url)
         if response.status_code == 200:
             newJsonData = response.json()
             jsonData += newJsonData
+            if len(newJsonData) < 50:
+                endOfJobs = True
+            if num in missedList:
+                missedList.remove(num)
+                failCounter = 0
         else:
             print("Received response: {} for page {}.".format(response.status_code, num))
-            missedList.append(num)
-        time.sleep(1)
+            if num not in missedList:
+                missedList.append(num)
+            failCounter += 1
+            if failCounter == 3:
+                break
+            # Code will try to get data from the page again for 3 attempts. It first sleeps again
+            # for a half of a second to give GitHub an idea that we are friendly.
+            # If it fails 3 times, it finally breaks out of the loop.
+            sleep(.5)
+            continue
+        sleep(.5)
+        num += 1
+
     if len(missedList) > 0:
         print("Missed data for pages {}.".format(str(missedList)[1:-1]))
     else:
         print("Successfully retrieved data from all pages.")
+
     return jsonData
 
 
 # Simple function that dumps JSON data to a .txt file.
-def dump_data(jobs, file_name):
+def dump_data(jobs: list, file_name: str):
+    if not (type(jobs) is list or type(jobs) is dict):
+        print("Illegal type of data. {} is of type {}. Please enter a list or dictionary.".format(jobs, type(jobs)))
+        return None
+    if type(jobs) is dict:
+        jobs = [jobs]
     with open(file_name, 'w') as openFile:
         for job in jobs:
             json.dump(job, openFile)
     print("Successfully dumped JSON data to {}.".format(file_name))
 
 
-# Simple function that dumps data to its corresponding column in the jobs.db database.
-def save_to_database(jobs):
+# Simple function that dumps data to its corresponding column in the database.
+def save_to_database(jobs: list, connection: sqlite3.Connection, cursor: sqlite3.Cursor):
     if not (type(jobs) is list or type(jobs) is dict):
         return None
     if type(jobs) is dict:
         jobs = [jobs]
-    connection = sqlite3.connect('jobs.db')
-    cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs(
-                   id TEXT PRIMARY KEY,
-                   Position_Type TEXT,
-                   URL TEXT,
-                   Created_at TEXT,
-                   Company TEXT,
-                   Company_URL TEXT,
-                   Location TEXT,
-                   Title TEXT,
-                   Description TEXT,
-                   How_To_Apply TEXT,
-                   Company_Logo TEXT
-                    )''')
     for job in jobs:
         if len(job) != 11:
             return None
         try:
-            cursor.execute("INSERT INTO jobs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [job['id'],
-                                                                                         job['type'],
-                                                                                         job['url'],
-                                                                                         job['created_at'],
-                                                                                         job['company'],
-                                                                                         job['company_url'],
-                                                                                         job['location'],
-                                                                                         job['title'],
-                                                                                         job['description'],
-                                                                                         job['how_to_apply'],
-                                                                                         job['company_logo'],
-                                                                                         ])
+            cursor.execute("INSERT INTO jobs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", [job['id'],
+                                                                                          job['type'],
+                                                                                          job['url'],
+                                                                                          job['created_at'],
+                                                                                          job['company'],
+                                                                                          job['company_url'],
+                                                                                          job['location'],
+                                                                                          job['title'],
+                                                                                          job['description'],
+                                                                                          job['how_to_apply'],
+                                                                                          job['company_logo']
+                                                                                          ])
         except sqlite3.IntegrityError:
-            print("Data already exists in the table..")
-        connection.commit()
+            print("Data already exists in the table.")
+    commit_db(connection)
+    print("Successfully stored all JSON data to jobs.db.")
+
+
+# Simple function that creates a connection with a filename given and returns a connection and cursor.
+def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    connection = sqlite3.connect(filename)  # Connects to an existing database or creates a new one.
+    cursor = connection.cursor()  # We are now ready to read/write data.
+    return connection, cursor
+
+
+# Simple function that closes a database.
+def close_db(connection: sqlite3.Connection):
+    connection.commit()
     connection.close()
+
+
+# Simple function that commits changes to a database.
+def commit_db(connection: sqlite3.Connection):
+    connection.commit()
+
+
+# Simple function that creates the initial table jobs with its respective columns.
+def create_table(connection: sqlite3.Connection, cursor: sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs(
+                       id TEXT PRIMARY KEY,
+                       Position_Type TEXT NOT NULL,
+                       URL TEXT NOT NULL,
+                       Created_at TEXT NOT NULL,
+                       Company TEXT NOT NULL,
+                       Company_URL TEXT,
+                       Location TEXT NOT NULL,
+                       Title TEXT NOT NULL,
+                       Description TEXT NOT NULL,
+                       How_To_Apply TEXT NOT NULL,
+                       Company_Logo TEXT
+                        );''')
+    commit_db(connection)
+
+
+# Simple function that cleans up SQL code by joining it together with only letters and numbers.
+def clean_sql(data: str):
+    return ''.join(char for char in data if char.isalnum())
 
 
 if __name__ == "__main__":
